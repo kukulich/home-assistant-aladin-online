95486f8bf336b27696699ad8cd241898c937f93c
Jaroslav Hanslík <kukulich@kukulich.cz>
2021-12-20 22:51:53 +0100

    Cleanup

--- custom_components/aladin_online/__init__.py
+++ custom_components/aladin_online/__init__.py
@@ -1,6 +1,5 @@
 from homeassistant import config_entries, core
-from homeassistant.components.sensor import DOMAIN as PLATFORM_SENSOR
-from homeassistant.components.weather import DOMAIN as PLATFORM_WEATHER
+from homeassistant.const import Platform
 from .aladin_online import AladinOnlineCoordinator
 from .const import DATA_COORDINATOR, DOMAIN
 
@@ -15,7 +14,7 @@ async def async_setup_entry(hass: core.HomeAssistant, config_entry: config_entri
 
 	hass.data[DOMAIN][config_entry.entry_id][DATA_COORDINATOR] = coordinator
 
-	for platform in (PLATFORM_SENSOR, PLATFORM_WEATHER):
+	for platform in (Platform.SENSOR, Platform.WEATHER):
 		hass.async_create_task(
 			hass.config_entries.async_forward_entry_setup(config_entry, platform)
 		)
