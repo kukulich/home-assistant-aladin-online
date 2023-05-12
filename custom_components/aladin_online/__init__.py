ae9f00292c651cb8d99b8aad4a814512bea72f10
Jaroslav Hanslík <kukulich@kukulich.cz>
2023-05-12 20:55:02 +0200

    Added async_unload_entry

--- custom_components/aladin_online/__init__.py
+++ custom_components/aladin_online/__init__.py
@@ -1,8 +1,13 @@
 from homeassistant import config_entries, core
 from homeassistant.const import Platform
+from typing import Final
 from .aladin_online import AladinOnlineCoordinator
 from .const import DATA_COORDINATOR, DOMAIN
 
+PLATFORMS: Final = [
+	Platform.SENSOR,
+	Platform.WEATHER,
+]
 
 async def async_setup_entry(hass: core.HomeAssistant, config_entry: config_entries.ConfigEntry) -> bool:
 	hass.data.setdefault(DOMAIN, {})
@@ -14,9 +19,13 @@ async def async_setup_entry(hass: core.HomeAssistant, config_entry: config_entri
 
 	hass.data[DOMAIN][config_entry.entry_id][DATA_COORDINATOR] = coordinator
 
-	for platform in (Platform.SENSOR, Platform.WEATHER):
-		hass.async_create_task(
-			hass.config_entries.async_forward_entry_setup(config_entry, platform)
-		)
+	await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+
+	return True
+
+async def async_unload_entry(hass: core.HomeAssistant, config_entry: config_entries.ConfigEntry) -> bool:
+	await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
+
+	hass.data[DOMAIN].pop(config_entry.entry_id)
 
 	return True
