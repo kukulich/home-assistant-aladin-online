4ccc5cd2a5774602e9615bee2d1b7960799615b2
Jaroslav Hanslík <kukulich@kukulich.cz>
2025-10-21 21:14:35 +0200

    Added reconfigure

--- custom_components/aladin_online/config_flow.py
+++ custom_components/aladin_online/config_flow.py
@@ -24,10 +24,7 @@ class AladinOnlineConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 				await self.async_set_unique_id(user_input[CONF_NAME])
 				self._abort_if_unique_id_configured()
 
-				session = aiohttp_client.async_get_clientsession(self.hass)
-				response = await session.get(URL.format(user_input[CONF_STATION_ID]))
-				if response.status != HTTPStatus.OK:
-					raise ServiceUnavailable
+				await self._async_validate_station(user_input[CONF_STATION_ID])
 
 				return self.async_create_entry(title=NAME, data=config)
 			except AbortFlow as ex:
@@ -46,3 +43,43 @@ class AladinOnlineConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 			}),
 			errors=errors,
 		)
+
+	async def async_step_reconfigure(self, user_input: Dict[str, Any] | None = None) -> FlowResult:
+		errors = {}
+		reconfigure_entry = self._get_reconfigure_entry()
+		if user_input is not None:
+			try:
+				config = {
+					CONF_NAME: reconfigure_entry.data[CONF_NAME],
+					CONF_STATION_ID: user_input[CONF_STATION_ID],
+				}
+
+				await self._async_validate_station(user_input[CONF_STATION_ID])
+
+				return self.async_update_reload_and_abort(reconfigure_entry, data=config)
+			except AbortFlow as ex:
+				return self.async_abort(reason=ex.reason)
+			except ServiceUnavailable:
+				errors["base"] = "service_unavailable"
+			except Exception:
+				LOGGER.error("Unknown error connecting to %s", URL.format(user_input[CONF_STATION_ID]))
+				return self.async_abort(reason="unknown")
+
+		if CONF_STATION_ID in reconfigure_entry.data:
+			station_id_field = vol.Required(CONF_STATION_ID, default=reconfigure_entry.data[CONF_STATION_ID])
+		else:
+			station_id_field = vol.Required(CONF_STATION_ID)
+
+		return self.async_show_form(
+			step_id="reconfigure",
+			data_schema=vol.Schema({
+				station_id_field: int,
+			}),
+			errors=errors,
+		)
+
+	async def _async_validate_station(self, station_id: int) -> None:
+		session = aiohttp_client.async_get_clientsession(self.hass)
+		response = await session.get(URL.format(station_id))
+		if response.status != HTTPStatus.OK:
+			raise ServiceUnavailable
