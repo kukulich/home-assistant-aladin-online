5312dc4ce7e09c81fc22447ab7c95a354df1dac0
ElvisEK <62078390+elvisek2020@users.noreply.github.com>
2026-06-08 07:53:11 +0200

    Update const.py
    Fix: migrate to new ČHMÚ API endpoint (data-provider.chmi.cz)

--- custom_components/aladin_online/const.py
+++ custom_components/aladin_online/const.py
@@ -2,7 +2,7 @@ import logging
 from typing import Final
 
 LOGGER: Final = logging.getLogger(__package__)
-
 DOMAIN: Final = "aladin_online"
-NAME: Final = "Aladin online (Czech Republic)"
-URL: Final = "https://aladinonline.oblacno.cz/get_data.php?latitude={}&longitude={}"
+NAME: Final = "Aladin online EK (Czech Republic)"
+URL: Final = "https://data-provider.chmi.cz/api/graphs/graf.meteogram/{}"
+CONF_STATION_ID: Final = "station_id"
